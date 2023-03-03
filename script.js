0f8952b502eb8b941e36772e1d59394d0218daf8
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-03-03 15:38:51 +0530

    First upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const shareButton = document.querySelector('.material-symbols-outlined');
+console.log(shareButton);
+const share = document.querySelector('.share');
+console.log(share);
+
+shareButton.addEventListener('mouseover', showShareOptions);
+
+function showShareOptions(){
+   share.classList.toggle('show');
+}
+
+function hideShareOptions(){
+    share.classList.remove('show');
+}
+
+window.addEventListener('click', hideShareOptions);
+
